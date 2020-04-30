ed0d5915f180eaadaf54109afe767c94f4a8142d
tclendo <33107696+tclendo@users.noreply.github.com>
2020-04-29 22:29:42 -0700

    bad ai

---
+++ Assets/AIController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AIController : MonoBehaviour {
+
+    public Transform Player;
+    int MoveSpeed = 4;
+    int MaxDist = 20;
+    //int MinDist = 5;
+ 
+    void Update() 
+    {
+        transform.LookAt(Player);
+ 
+        if (Vector3.Distance(transform.position, Player.position) <= MaxDist) {
+ 
+            transform.position += transform.forward * MoveSpeed * Time.deltaTime;
+
+        }
+    }
+}
