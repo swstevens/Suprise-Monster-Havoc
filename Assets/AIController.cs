fb5c178e90e52d0e30c78aa334dffb4c2fd1c259
swstevens <sstevensw@gmail.com>
2020-05-13 11:33:43 -0700

    updating enemy AI to navmeshes

--- Assets/AIController.cs
+++ Assets/AIController.cs
@@ -2,22 +2,40 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.AI;
 
 public class AIController : MonoBehaviour {
 
-    public Transform Player;
-    int MoveSpeed = 4;
-    int MaxDist = 20;
+    Transform target;
+    NavMeshAgent agent;
+
+    public float MoveSpeed = 4f;
+    public float MaxDist = 12f;
+    public float distance;
     //int MinDist = 5;
+
+    void Start() 
+    {
+    	agent = GetComponent<NavMeshAgent>();
+    	target = PlayerManager.instance.player.transform;
+    }
  
     void Update() 
     {
-        transform.LookAt(Player);
+        //transform.LookAt(Player);
+        distance = Vector3.Distance(target.position, transform.position);
+
  
-        if (Vector3.Distance(transform.position, Player.position) <= MaxDist) {
- 
-            transform.position += transform.forward * MoveSpeed * Time.deltaTime;
+        if (distance <= MaxDist) {
+ 			agent.SetDestination(target.position);
+            //transform.position += transform.forward * MoveSpeed * Time.deltaTime;
 
         }
+    }
+
+    void OnDrawGizmosSelected()
+    {
+    	Gizmos.color = Color.red;
+    	Gizmos.DrawWireSphere(transform.position, MaxDist);
     }
 }
