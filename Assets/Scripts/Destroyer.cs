8251f21eb7d5b7c32934f61a9372a90ff1140221
swstevens <sstevensw@gmail.com>
2020-05-26 21:06:49 -0700

    map generation tweaks
    fixed most of the issues with doors that lead to nowhere, but now getting some strange issues with extra walls being placed  which blocks off parts of the map. This has to do with the order of collision detections during the fixedupdate stage, and I'm not sure how to get unity to prioritize one triggerenter over another

--- Assets/Scripts/Destroyer.cs
+++ Assets/Scripts/Destroyer.cs
@@ -7,9 +7,11 @@ public class Destroyer : MonoBehaviour
     // Start is called before the first frame update
     void OnTriggerEnter(Collider other)
     {
+    	// Destroy(other.gameObject);
 		if (other.CompareTag("SpawnPoint"))
         {
         	Destroy(other.gameObject);
+        	Debug.Log("Destroyed a node");
         }
     }
 }
--- Assets/Scripts/roomSpawner.cs
+++ Assets/Scripts/roomSpawner.cs
@@ -22,9 +22,10 @@ public class roomSpawner : MonoBehaviour
     void Start()
     {
         //Time.timeScale = 1f;
-        Destroy(gameObject, waitTime);
+        // Destroying all nodes after five seconds appears to be causing problems (not every node has bee fully spawned before nodes are deleted)
+        //Destroy(gameObject, waitTime);
         templates = GameObject.FindGameObjectWithTag("Rooms").GetComponent<RoomTemplates>();
-        Invoke("Spawn", 0.1f);
+        Invoke("Spawn", 1f);
     }
     
     void Spawn()
@@ -44,6 +45,7 @@ public class roomSpawner : MonoBehaviour
                     rand = Random.Range(0, templates.bottomRooms.Length);
                     Instantiate(templates.bottomRooms[rand], transform.position, templates.bottomRooms[rand].transform.rotation);
                     MapManager.instance.i++;
+                    Debug.Log("Bottom Door Spawned (0 & below limit) 0->2");
             }
             else if (openingDirection == 2)
             {
@@ -51,6 +53,7 @@ public class roomSpawner : MonoBehaviour
                     rand = Random.Range(0, templates.topRooms.Length);
                     Instantiate(templates.topRooms[rand], transform.position, templates.topRooms[rand].transform.rotation);
                     MapManager.instance.i++;
+                    Debug.Log("Top Door Spawned (0 & below limit) 0->2");
             }
             else if (openingDirection == 3)
             {
@@ -58,6 +61,7 @@ public class roomSpawner : MonoBehaviour
                     rand = Random.Range(0, templates.leftRooms.Length);
                     Instantiate(templates.leftRooms[rand], transform.position, templates.leftRooms[rand].transform.rotation);
                     MapManager.instance.i++;
+                    Debug.Log("Left Door Spawned (0 & below limit) 0->2");
             }
             else if (openingDirection == 4)
             {
@@ -65,6 +69,7 @@ public class roomSpawner : MonoBehaviour
                     rand = Random.Range(0, templates.rightRooms.Length);
                     Instantiate(templates.rightRooms[rand], transform.position, templates.rightRooms[rand].transform.rotation);
                     MapManager.instance.i++;
+                    Debug.Log("Right Door Spawned (0 & below limit) 0->2");
             } 
             spawned = 2;
         }
@@ -72,6 +77,7 @@ public class roomSpawner : MonoBehaviour
         {
         	// mark all leftover doors to nowhere as needing an end room
             spawned = 1;
+            Debug.Log("Leftover room set to 2");
         }
         // if spawn == 1, then spawn an end room
         if (spawned == 1)
@@ -81,24 +87,28 @@ public class roomSpawner : MonoBehaviour
                 // spawn a bottom end room
                 rand = Random.Range(0, templates.bottomEndRooms.Length);
                 Instantiate(templates.bottomEndRooms[rand], transform.position, templates.bottomEndRooms[rand].transform.rotation);
+                Debug.Log("Bottom End Spawned 1->2");
             }
             else if (openingDirection == 2)
             {
                 // spawn a top end room
                 rand = Random.Range(0, templates.topEndRooms.Length);
                 Instantiate(templates.topEndRooms[rand], transform.position, templates.topEndRooms[rand].transform.rotation);
+                Debug.Log("Top End Spawned 1->2");
             }
             else if (openingDirection == 3)
             {
                 // spawn a left end room
                 rand = Random.Range(0, templates.leftEndRooms.Length);
                 Instantiate(templates.leftEndRooms[rand], transform.position, templates.leftEndRooms[rand].transform.rotation);
+                Debug.Log("Left End Spawned 1->2");
             }
             else if (openingDirection == 4)
             {
                 // spawn a right end room
                 rand = Random.Range(0, templates.rightEndRooms.Length);
                 Instantiate(templates.rightEndRooms[rand], transform.position, templates.rightEndRooms[rand].transform.rotation);
+                Debug.Log("Right End Spawned 1->2");
             } 
             spawned = 2;
         }
@@ -106,6 +116,11 @@ public class roomSpawner : MonoBehaviour
 
     void OnTriggerEnter(Collider other)
     {
+    	if (other.CompareTag("Destroyer"))
+    	{
+    		spawned = 2;
+    	}
+
         if (other.CompareTag("NoRoom"))
         {
             // spawn a wall object instead of a room
@@ -115,6 +130,7 @@ public class roomSpawner : MonoBehaviour
         
         if (other.CompareTag("SpawnPoint"))
         {
+        	// they need to be 
             if (other.GetComponent<roomSpawner>().spawned == 0 && spawned == 0)
             {
             	// spawn a wall
@@ -122,30 +138,42 @@ public class roomSpawner : MonoBehaviour
             	{
                 	// spawn a bottom end room
                 	rand = Random.Range(0, templates.bottomEndRooms.Length);
-                	Instantiate(templates.bottomEndRooms[rand], transform.position, templates.bottomEndRooms[rand].transform.rotation);
+                	Instantiate(templates.bottomEndRooms[0], transform.position, templates.bottomEndRooms[rand].transform.rotation);
+                	Debug.Log("Bottom End Spawned(collision encountered) 0->2");
             	}
             	else if (openingDirection == 2)
             	{
                 	// spawn a top end room
                 	rand = Random.Range(0, templates.topEndRooms.Length);
-                	Instantiate(templates.topEndRooms[rand], transform.position, templates.topEndRooms[rand].transform.rotation);
+                	Instantiate(templates.topEndRooms[0], transform.position, templates.topEndRooms[rand].transform.rotation);
+                	Debug.Log("Top End Spawned(collision encountered) 0->2");
             	}
             	else if (openingDirection == 3)
             	{
                 	// spawn a left end room
                 	rand = Random.Range(0, templates.leftEndRooms.Length);
-                	Instantiate(templates.leftEndRooms[rand], transform.position, templates.leftEndRooms[rand].transform.rotation);
+                	Instantiate(templates.leftEndRooms[0], transform.position, templates.leftEndRooms[rand].transform.rotation);
+                	Debug.Log("Left End Spawned(collision encountered) 0->2");
             	}
             	else if (openingDirection == 4)
             	{
                 	// spawn a right end room
                 	rand = Random.Range(0, templates.rightEndRooms.Length);
-                	Instantiate(templates.rightEndRooms[rand], transform.position, templates.rightEndRooms[rand].transform.rotation);
+                	Instantiate(templates.rightEndRooms[0], transform.position, templates.rightEndRooms[rand].transform.rotation);
+                	Debug.Log("Right End Spawned(collision encountered) 0->2");
             	} 
                 // Instantiate(templates.closedRoom, transform.position, Quaternion.identity);
                 Destroy(gameObject);
+                // putting this inside the loop solves another issue of doors to nowhere
+                spawned = 2;
+                // should solve the doors to nowhere that are right next to one another
+                other.GetComponent<roomSpawner>().spawned = 1;
             }
-            spawned = 2;
+            // This is causing errors 
+            if ((other.GetComponent<roomSpawner>().spawned == 1 || other.GetComponent<roomSpawner>().spawned == 2) && spawned == 0)
+            {
+                spawned = 1;
+            }
         }
     }
 
