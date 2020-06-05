6a1c81181dfcb4737659353f04cbfb082f609d59
tclendo <33107696+tclendo@users.noreply.github.com>
2020-06-05 13:17:51 -0700

    options is dontdestroyonload

---
+++ Assets/DontDestroyOptions.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DontDestroyOptions : MonoBehaviour
+{
+
+	public GameObject options;
+	private static GameObject optionsCheck;
+
+    void Awake()
+    {
+
+        DontDestroyOnLoad(options);
+
+        if (optionsCheck == null) {
+
+        	optionsCheck = options;
+
+        } else {
+
+        	Destroy(options);
+        }
+    }
+}
